ce66665a542f51f01c02fdc89282d23c0bbdc76f
ephyg <ephywaanofii@gmail.com>
2023-07-30 01:01:05 +0300

    new feature:add Proforma page 	-create proforma page 	-create add popup 	-create edit popup 	but not finished at all

--- src/components/common/button/Button.jsx
+++ src/components/common/button/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Button = ({ className, text, icon }) => {
+const Button = ({ className, text, icon, onClick }) => {
   return (
     <div className="flex items-stretch justify-center">
       <button
         className={`font-roboto font-bold text-lg text-white items-stretch flex ${className}`}
+        onClick={onClick}
       >
         <div className="flex justify-center items-center gap-2">
           {icon != null &&
