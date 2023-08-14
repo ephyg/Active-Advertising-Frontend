1a652eeb932253ea0855401e229619606e2a50df
ephyg <ephywaanofii@gmail.com>
2023-08-15 00:01:32 +0300

    add new feature: formik validation for proforma addpopup and updatepopup

--- src/pages/common/addProforma/UpdatePopUp.jsx
+++ src/pages/common/addProforma/UpdatePopUp.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { TiDelete } from "react-icons/ti";
 import InputField from "../../../components/common/inputField/InputField";
 import Button from "../../../components/common/button/Button";
+import { useFormik } from "formik";
 
 function UpdatePopUp({ Open, id, data, onClick }) {
   const [itemDescription, setitemDescription] = useState(
@@ -12,13 +13,14 @@ function UpdatePopUp({ Open, id, data, onClick }) {
   const [vendor, setVendor] = useState(data[id]["vendor"]);
   const [price, setPrice] = useState(data[id]["price"]);
   const [totalPrice, setTotalPrice] = useState(data[id]["totalPrice"]);
+  const [profitPrice, setProfitPrice] = useState(data[id]["profitPrice"]);
 
   const handleUpdateClick = () => {
     setTotalPrice(Number(quantity) * Number(price));
 
     const updatedData = {
       id: data[id]["id"],
-      itemDecription: itemDescription,
+      itemDecription: data[id]["itemDescription"],
       size: size,
       vendor: vendor,
       quantity: quantity,
@@ -29,8 +31,42 @@ function UpdatePopUp({ Open, id, data, onClick }) {
     onClick(data);
     Open(false);
   };
+
+  const onSubmit = () => {
+    const updatedData = {
+      id: data[id]["id"],
+      itemDecription: values.itemDescription,
+      size: values.size,
+      vendor: values.vendor,
+      quantity: values.quantity,
+      unitPrice: values.unitPrice,
+      totalPrice: Number(values.quantity) * Number(values.unitPrice),
+      profitPrice: values.profitPrice,
+    };
+     data[id] = updatedData;
+    onClick(data);
+    Open(false);
+  };
+  console.log(data);
+  const { values, errors, touched, handleChange, handleBlur, handleSubmit } =
+    useFormik({
+      initialValues: {
+        itemDescription: data[id].itemDecription,
+        size: data[id].size,
+        vendor: data[id].vendor,
+        quantity: data[id].quantity,
+        unitPrice: data[id].unitPrice,
+        totalPrice: data[id].totalPrice,
+        profitPrice: data[id].profitPrice,
+      },
+      // validationSchema: AddValidation,
+      onSubmit,
+    });
   return (
-    <form className="flex flex-col bg-white px-10 shadow-2xl py-6 rounded-2xl">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col bg-white px-10 shadow-2xl py-6 rounded-2xl md:px-2"
+    >
       <div className="flex  items-start justify-between mb-5">
         <h1 className="text-blue font-roboto font-bold text-base">Add Order</h1>
         <TiDelete
@@ -40,60 +76,76 @@ function UpdatePopUp({ Open, id, data, onClick }) {
           onClick={() => Open(false)}
         />
       </div>
-      <div className="flex-col flex gap-3">
-        <div className="flex flex-row gap-5 overflow-hidden justify-between">
-          <InputField
-            label="Items Descritpion"
-            placeholder="item"
-            className="py-1"
-            value={itemDescription}
-            onChange={(e) => {
-              setitemDescription(e.target.value);
-            }}
-          />
-          <InputField
-            label="Size"
-            placeholder="quantity"
-            className="py-1"
-            value={size}
-            onChange={(e) => {
-              setSize(e.target.value);
-            }}
-          />
-        </div>
-        <div className="flex flex-row gap-5 overflow-hidden justify-between">
-          <InputField
-            label="Quantity"
-            placeholder="quantity"
-            className="py-1"
-            value={quantity}
-            onChange={(e) => {
-              setQuantity(e.target.value);
-            }}
-          />
-          <InputField
-            label="Price"
-            placeholder="price"
-            className="py-1"
-            value={price}
-            onChange={(e) => {
-              setPrice(e.target.value);
-            }}
-          />
-        </div>
-        <div className="flex flex-row gap-5 justify-between mb-3">
-          <InputField
-            label="Vendor"
-            placeholder="Vendor Name"
-            className="py-1"
-            value={vendor}
-            onChange={(e) => setVendor(e.target.value)}
-          />
-        </div>
+      <div className="grid grid-cols-2 gap-3 justify-center items-center">
+        <InputField
+          label="Items Descritpion"
+          className="py-1 md:h-8 md:text-sm"
+          id="itemDescription"
+          name="itemDescription"
+          value={values.itemDescription}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={
+            errors.itemDescription && touched.itemDescription
+              ? errors.itemDescription
+              : ""
+          }
+        />
+        <InputField
+          label="Size"
+          id="size"
+          name="size"
+          className="py-1 md:h-8 md:text-sm"
+          value={values.size}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={errors.size && touched.size ? errors.size : ""}
+        />
+        <InputField
+          label="Quantity"
+          className="py-1 md:h-8 md:text-sm"
+          value={quantity}
+          onChange={(e) => {
+            setQuantity(e.target.value);
+          }}
+        />
+        <InputField
+          label="Price"
+          className="py-1 md:h-8 md:text-sm"
+          id="quantity"
+          name="quantity"
+          value={values.quantity}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={errors.quantity && touched.quantity ? errors.quantity : ""}
+        />
+        <InputField
+          label="Vendor"
+          className="py-1 md:h-8 md:text-sm"
+          id="vendor"
+          name="vendor"
+          value={values.vendor}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={errors.vendor && touched.vendor ? errors.vendor : ""}
+        />
+        <InputField
+          label="Profit"
+          className="py-1 md:h-8 md:text-sm"
+          id="profitPrice"
+          name="profitPrice"
+          value={values.profitPrice}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          error={
+            errors.profitPrice && touched.profitPrice ? errors.profitPrice : ""
+          }
+        />
+      </div>
+      <div className="w-full justify-center items-center mt-3">
         <Button
           text="Update Order"
           className="bg-blue text-white py-1 px-3 rounded-md hover:bg-blue_hover"
-          onClick={handleUpdateClick}
         />
       </div>
     </form>
